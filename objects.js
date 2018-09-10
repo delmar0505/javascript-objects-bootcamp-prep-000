f1a9398cdbdf680615266c98a20ebc0ca9abe48f
oscar leon <first.liberal.thinker@gmail.com>
2018-09-10 22:43:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,5 @@
 var playlist = { artistName:'song name'};
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName01] = 'song2';
-}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
